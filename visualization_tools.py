a1143af2ee1ea3023dc1adb7afd6e46302e9fca8
jweiae <42104505+jweiae@users.noreply.github.com>
2022-06-13 00:55:59 -0500

    debug

--- visualization_tools.py
+++ visualization_tools.py
@@ -290,7 +290,7 @@ if __name__ == '__main__':
     im_list.append(im.convert('RGB'))
     img_list,_ = transform1(im_list, target=None,num_frames=1)
     img=torch.cat(img_list,dim=0)
-    #img=img.unsqueeze(0)
+    img=img.unsqueeze(0)
     print(img.shape)
     abc=torch.Tensor([1,2,3]).cuda()
     print(abc)
